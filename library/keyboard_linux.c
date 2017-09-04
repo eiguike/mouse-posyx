d08ac450db7157eff45d6b3107a750f72a6eff69
Henrique <rick.eihara@gmail.com>
2017-09-04 18:21:12 -0300

    fix: space/backspace and enter works on linux

--- library/keyboard_linux.c
+++ library/keyboard_linux.c
@@ -2,19 +2,30 @@
 
 #include <X11/Xlib.h>
 #include <X11/extensions/XTest.h>
+#include <X11/keysymdef.h>
 
+#define XK_KP_Space                      0xff80  /* Space */
+#define XK_KP_Enter                      0xff8d  /* Enter */
+#define XK_BackSpace                     0xff08  /* Back space, back char */
+#define XK_space                         0x0020
 
 void TypeLetterLinux (KEYBOARD * this, char * Input) {
-
-  return;
   printf("TypeLetterLinux begin\n");
   printf("Letter received: %s\n", Input);
   KeyCode Keycode = 0;
 
-  Keycode = XKeysymToKeycode(this->Display, XStringToKeysym(Input));
+  if (strcmp(Input, "Backspace") == 0) {
+    Keycode = XKeysymToKeycode(this->Display, XK_BackSpace);
+  } else if (strcmp(Input, " ") == 0) {
+    Keycode = XKeysymToKeycode(this->Display, XK_space);
+  } else if (strcmp(Input, "Enter") == 0) {
+    Keycode = XKeysymToKeycode(this->Display, XK_KP_Enter);
+  } else {
+    Keycode = XKeysymToKeycode(this->Display, XStringToKeysym(Input));
+  }
 
   if (Keycode == NoSymbol) {
-    printf("broke letter\n");
+    printf("Could not translate this Input: %s to a Keycode\n", Input);
     return;
   }
 
