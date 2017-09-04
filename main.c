33e9aacfd304e7ce95418716a5738eaf9b93f4d4
Henrique <rick.eihara@gmail.com>
2017-09-03 21:41:48 -0300

    feat: initialize keyboard device

--- main.c
+++ main.c
@@ -2,12 +2,15 @@
 #include <stdlib.h>
 
 #include "mouse.h"
+#include "keyboard.h"
 #include "server.h"
 
 MOUSE * gMouse = NULL;
+KEYBOARD * gKeyboard = NULL;
 
 int main() {
   gMouse = InitializeMouseDevice ();
+  gKeyboard = InitializeKeyboardDevice ();
   SERVER * Server = InitializeServer(8000);
   
   Server->Start(Server);
