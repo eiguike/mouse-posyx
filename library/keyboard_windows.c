75e1a8130ae96751a73626162867334345d53d5d
Rick <rick.eihara@gmail.com>
2017-09-04 22:37:50 -0300

    feat: implement windows keyboard, not ready yet, only ascii characters yet

--- library/keyboard_windows.c
+++ library/keyboard_windows.c
@@ -1,8 +1,35 @@
 #include "keyboard.h"
+#include <windows.h>
 
 void TypeLetterWindows (KEYBOARD * this, char * Input) {
   printf("TypeLetterWindows begin\n");
   printf("Letter received: %s\n", Input);
+
+  INPUT InputCommand;
+
+  memset(&InputCommand, 0, sizeof(InputCommand));
+  InputCommand.type = INPUT_KEYBOARD;
+  InputCommand.ki.wScan = 0;
+  InputCommand.ki.time = 0;
+  InputCommand.ki.dwExtraInfo = 0;
+  InputCommand.ki.wVk = 0x41;
+  InputCommand.mi.dwFlags = KEYEVENTF_SCANCODE;
+
+  if (strcmp(Input, "Backspace") == 0) {
+    InputCommand.ki.wVk = 0x08;
+  } else if (strcmp(Input, " ") == 0) {
+    InputCommand.ki.wVk = 0x20;
+  } else if (strcmp(Input, "Enter") == 0) {
+    InputCommand.ki.wVk = 0x0D;
+  } else {
+  	printf("Valor: %d\n", Input[0]);
+    InputCommand.ki.wVk = 0x41 + (Input[0] - 65);
+  }
+
+  SendInput(1, &InputCommand, sizeof(INPUT));
+
+  InputCommand.mi.dwFlags = KEYEVENTF_KEYUP;
+  
   printf("TypeLetterWindows end\n");
 }
 
