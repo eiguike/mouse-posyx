8dc218d282fda535710ff8dac70694a9a1bfec2c
Henrique <rick.eihara@gmail.com>
2017-09-14 21:00:32 -0300

    refact: move some data to specific files

--- include/keyboard.h
+++ include/keyboard.h
@@ -11,6 +11,33 @@ typedef Display DISPLAY;
 typedef Window WINDOW;
 #endif
 
+enum Accent {
+  ACUTE,
+  CEDILLA,
+  CIRCUMFLEX,
+  DIAERESIS,
+  GRAVE,
+  TILDE,
+  NOTHING
+};
+
+typedef enum Accent ACCENT;
+
+typedef struct LetterAccent {
+  char * Letters;
+  ACCENT Accent;
+  unsigned int Capitalized;
+  unsigned int Composed;
+  KeySym Keysym;
+  char * Type;
+} LETTER_ACCENT;
+
+typedef struct LetterAccentToLetter {
+  char * AccentLetters;
+  unsigned int Capitalized;
+  KeySym Letter;
+} LETTER_ACCENT_TO_LETTER;
+
 typedef struct KEYBOARD_DEVICE {
 
 #ifdef LINUX
