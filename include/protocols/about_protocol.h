eb355c330d5c92043559bc52b5ccc5f6f42815b4
Henrique <rick.eihara@gmail.com>
2017-11-13 22:12:29 -0200

    feat: add about protocol for ipless connection

--- include/protocols/about_protocol.h
+++ include/protocols/about_protocol.h
@@ -1,7 +1,23 @@
 #include "server.h"
 
+typedef struct buffer {
+	//unsigned char data[ LWS_SEND_BUFFER_PRE_PADDING +
+  //                    EXAMPLE_RX_BUFFER_BYTES +
+  //                    LWS_SEND_BUFFER_POST_PADDING ];
+  unsigned char * Data
+	size_t Length;
+} BUFFER;
+
+typedef struct Information {
+  char * Name;
+  char * Linux;
+  char * ApplicationVersion;
+} INFORMATION;
+
 int AboutCallback (struct lws *wsi,
                    enum lws_callback_reasons reason,
                    void * user,
                    void * in,
                    size_t len);
+
+void InitializeAboutProtocol ();
